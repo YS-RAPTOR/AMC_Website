5238a4be8c1753be1a0d97db6ae72b712d45bd82
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2023-03-14 21:55:18 +1100

    Hero Section Skeleton Done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#F52200",
+                comp1: "#DE0B14",
+                comp2: "#DE460B",
+                comp3: "#FA720D",
+                comp4: "#FA0D8E",
+                background: "#e9e9e9",
+            },
+        },
+    },
+    plugins: [],
+};
